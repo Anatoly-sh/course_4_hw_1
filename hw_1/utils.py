ef84d99b0707a51415d5cd2b7f3a268a832e80ae
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-02-21 20:44:26 +0300

    Задание выполнено, тесты работают

---
+++ hw_1/utils.py
@@ -0,0 +1,35 @@
+class Item:
+    """Класс вида товара с наименованием, ценой и количеством"""
+    discount_rate = 0.85        # уровень цен с учетом скидки
+    items_list = []             # хранение созданных экземпляров класса
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def kind_amount(self):
+        """получить общую стоимость конкретного товара в магазине"""
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        """применить установленную скидку для конкретного товара"""
+        return self.price * Item.discount_rate  # дисконтная стоимость
+
+
+if __name__ == '__main__':
+    item1 = Item("Смартфон", 10000, 20)
+    Item.items_list.append(item1)
+    item2 = Item("Ноутбук", 20000, 5)
+    Item.items_list.append(item2)
+
+    print(item1.kind_amount())
+    print(item2.kind_amount())
+
+    Item.discount_rate = 0.8  # устанавливаем новый уровень цен
+
+    print(item1.price)
+    print(item1.apply_discount())
+    print(Item.items_list[0].name)
+    print(Item.items_list[0].price)
+    print(Item.items_list)
